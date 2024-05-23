410e249241970bc9e3548546dfe3d67c49bbcb98
MaxP-777 <porollomax@mail.ru>
2024-05-24 00:02:17 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Number_of_completed_remote_sensing = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_per_task = (Number_of_hours_spent/Number_of_completed_remote_sensing)
+print('Курс:', Course_name,', ' 'всего задач: 12,', 'затрачено часов:', Number_of_hours_spent,',' 'среднее время выполнения',  Time_per_task, 'часа')
